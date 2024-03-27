d219cdc18d63262a8a08ef8069ce83e7e60ddaef
xusniya123 <xusniya12345678@gmail.com>
2024-03-27 13:49:34 +0500

    lesson 4 moth (10)

---
+++ main.js
@@ -0,0 +1,38 @@
+// function love(){
+//     console.log("I love Uzbekiston");
+// }
+// love();
+// love();
+// love();
+
+
+// let math = () =>{
+//     console.log(Math.pow(5,7));
+// }
+
+
+// math();
+
+// let hisob = () => {
+//     console.log(10*10);
+// }
+
+// hisob();
+
+// let myFunction = function () {
+    
+// }
+
+
+
+
+
+function max(a,b) {
+    let c;
+    c = Math.max(a,b);
+    alert("kiritilgan 2 ta sonning kattasi = " + c);
+}
+
+let birinchi = Number(prompt("1 chi soni kiriting..."));
+let ikkinchi = Number(prompt("2 chi soni kiriting..."));
+max(birinchi,ikkinchi);
